001cba3e016d3a7a706daf42345b2303b5e64751
Louis Desportes <louis@akkes.fr>
2020-12-01 18:49:43 +0100

    faster 1a

--- 1/solver.jl
+++ 1/solver.jl
@@ -1,14 +1,17 @@
 using DelimitedFiles
 using Printf
 
+
 function product_of_year_sum(matrix)
     which_mask = [2020 == (a + b) for a in matrix, b in matrix]
-    results = [a * b for a in matrix, b in matrix] .* which_mask
-    return sum(results) / 2 # answer was selected two times
+    pos = findfirst(which_mask)
+    return matrix[pos[1]] * matrix[pos[2]]
 end
 
 matrix = [1721; 979; 366; 299; 675; 1456]
 @printf("%d\n", product_of_year_sum(matrix))
+# 514579
 
-matrix = readdlm("input")
+matrix = readdlm("input")[:,1] # throw second dim
 @printf("%d\n", product_of_year_sum(matrix))
+# 1013211
