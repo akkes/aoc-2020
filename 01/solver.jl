cf672f7894c3befd9d3f100a89387289e18f45f5
Louis Desportes <louis@akkes.fr>
2020-12-10 12:00:25 +0100

    golf day1

--- 01/solver.jl
+++ 01/solver.jl
@@ -11,27 +11,44 @@ function product_of_year_sum(matrix::Array{Int})
 end
 
 function product_product_of_year_sum(matrix::Array{Int})
-    for items in Iterators.product(matrix, matrix, matrix)
-        a, b, c = items
-        if a + b + c == 2020
-            return a * b * c
+    for a in matrix
+        for b in matrix
+            if a + b >= 2020
+                continue
+            end
+            for c in matrix
+                if a + b + c == 2020
+                    return a * b * c
+                end
+            end
         end
     end
 end
 
-demo = Int[1721 979 366 299 675 1456]
-println(product_of_year_sum(demo))
-# 514579
-println(product_product_of_year_sum(demo))
-# 241861950
+function main()
+    demo = Int[1721 979 366 299 675 1456]
+    input = readdlm("input", Int)[:,1] # throw second dim
+    result = product_of_year_sum(demo)
+    println(result)
+    @assert (result == 514579)
+
+    result = product_product_of_year_sum(demo)
+    println(result)
+    @assert (result == 241861950)
 
 
-input = readdlm("input", Int)[:,1] # throw second dim
-println(product_of_year_sum(input))
-# 1013211
-println(product_product_of_year_sum(input))
-# 13891280
+    input = readdlm("input", Int)[:,1] # throw second dim
+    result = product_of_year_sum(input)
+    println(result)
+    @assert (result == 1013211)
+    
+    result = product_product_of_year_sum(input)
+    println(result)
+    @assert (result == 13891280)
 
-benchmark = readdlm("benchmark", Int)[:,1] 
-@btime product_of_year_sum(benchmark)
-@btime product_product_of_year_sum(benchmark)
+    benchmark = readdlm("benchmark", Int)[:,1] 
+    @btime product_of_year_sum($benchmark)
+    @btime product_product_of_year_sum($benchmark)
+end
+
+main()
